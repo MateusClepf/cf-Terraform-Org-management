94eece50583a6d970fb5d5e053189d719a87f452
MateusClepf <mateusclepf@gmail.com>
2025-03-07 16:24:26 +0100

    Chore: First commit, accounts, zones and modules structure

---
+++ accounts/account-2/tf-01.whereismypacket.net/main.tf
@@ -0,0 +1,70 @@
+resource "cloudflare_zone" "zone" {
+  account_id  = var.cloudflare_account_tag
+  paused      = false
+  zone        = var.cloudflare_zone_name
+  type        = var.cloudflare_zone_type
+  plan        = var.cloudflare_zone_plan
+}
+
+resource "cloudflare_zone_settings_override" "zone-settings" {
+  zone_id = cloudflare_zone.zone.id
+  settings {
+    always_use_https            = "on"
+    browser_cache_ttl           = 0
+    browser_check               = "on"
+    http2                       = "on"
+    h2_prioritization           = "off"
+    http3                       = "off"
+    image_resizing              = "on"
+    ip_geolocation              = "on"
+    ipv6                        = "on"
+    mirage                      = "on"
+    prefetch_preload            = "on"
+    security_header {
+      enabled            = false
+    }
+    ssl                         = "full"
+    tls_1_3                     = "zrt"
+    true_client_ip_header       = "on"
+    websockets                  = "on"
+    zero_rtt                    = "on"
+    min_tls_version             = "1.2"
+    polish            = "lossless"
+    webp              = "on"
+  }
+}
+
+resource "cloudflare_argo" "argo" {
+  zone_id        = cloudflare_zone.zone.id
+  smart_routing  = "on"
+}
+
+resource "cloudflare_tiered_cache" "tiered-cache" {
+  zone_id    = cloudflare_zone.zone.id
+  cache_type = "generic"
+}
+
+resource "cloudflare_regional_tiered_cache" "regional-tiered-cache" {
+  zone_id = cloudflare_zone.zone.id
+  value   = "on"
+}
+
+resource "cloudflare_url_normalization_settings" "url-normalization" {
+  zone_id = cloudflare_zone.zone.id
+  scope   = "incoming"
+  type    = "cloudflare"
+}
+
+module "cloudflare_bot_management" {
+  source = "github.com/MateusClepf/cf-Terraform-Org-management/modules/cloudflare-bot-management?ref=v1.0.0"
+  
+  zone_id = var.zone_id
+}
+/*
+resource "cloudflare_bot_management" "bot-management" {
+  zone_id                = cloudflare_zone.zone.id
+  ai_bots_protection     = "disabled"
+  auto_update_model      = true
+  enable_js              = true
+  suppress_session_score = false
+}*/
